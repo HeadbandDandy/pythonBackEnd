dadbe3de2881ce9304260f613e4e0e062cd5a0df
Darryl Johnson <dandycodes@gmail.com>
2022-11-25 13:10:27 -0500

    dashboard route tested successfully

--- app/__init__.py
+++ app/__init__.py
@@ -8,9 +8,6 @@ def create_app(test_config=None):
       # sets up application configuration
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
-  # below registers the "home, dashboard routes" blueprint 
-  app.register_blueprint(home)
-  app.register_blueprint(dashboard)
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
@@ -20,4 +17,8 @@ def create_app(test_config=None):
     return 'hello world'
 
 
+  # below registers the "home, dashboard routes" blueprint 
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
+  
   return app
