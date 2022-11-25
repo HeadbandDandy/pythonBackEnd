09516f00e2e4330dd3d81981c51c1aedd6e7dec8
Darryl Johnson <dandycodes@gmail.com>
2022-11-25 12:54:13 -0500

    home route tested successfully

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
+# below imports the routes
+from app.routes import home
+
+
+# below imports the flask package
 from flask import Flask
 
 def create_app(test_config=None):
       # sets up application configuration
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
+  # below registers the "home routes" blueprint 
+  app.register_blueprint(home)
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
@@ -11,5 +18,6 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
+
 
   return app
