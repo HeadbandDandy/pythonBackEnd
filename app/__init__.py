8d736f01e565ff3921d8a9a7706a3216c107ba29
Darryl Johnson <dandycodes@gmail.com>
2022-11-25 12:26:22 -0500

    route tested

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 
 def create_app(test_config=None):
-  # sets up application configuration
+      # sets up application configuration
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
+# contains display from routing
+  @app.route('/hello')
+  def hello():
+    return 'hello world'
 
   return app
