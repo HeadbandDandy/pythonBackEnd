a4196cb7f32c6f75824d256158bc71f7ab51a08f
Darryl Johnson <dandycodes@gmail.com>
2022-11-25 11:57:55 -0500

    server file set up

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app(test_config=None):
+  # sets up application configuration
+  app = Flask(__name__, static_url_path='/')
+  app.url_map.strict_slashes = False
+  app.config.from_mapping(
+    SECRET_KEY='super_secret_key'
+  )
+
+  return app
