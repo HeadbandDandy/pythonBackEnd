887f13ecebde47f66e19c8a4a9d3ff8e1bd3d2fc
Darryl Johnson <dandycodes@gmail.com>
2022-11-29 13:59:13 -0500

    Post model created needs testing and querying

---
+++ app/models/Post.py
@@ -0,0 +1,19 @@
+# below contains imports needed for Post Model
+
+from datetime import datetime
+from email.policy import default
+from app.db import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+# below contains the Post class
+
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    post_url = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    
