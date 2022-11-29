04831c5b0cf04d2787ce6cbabd4aab108ab9da41
Darryl Johnson <dandycodes@gmail.com>
2022-11-29 08:37:19 -0500

    email validation added

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,11 @@
+from typing_extensions import assert_type
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+# below allows for validation of user data before acceptance
+from sqlalchemy.orm import validates
+
+
+
 
 # class for user placed below
 
@@ -9,3 +15,12 @@ class User(Base):
     username = Column(String(50), nullable=False)
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(100), nullable=False)
+
+# below validates for emails and ensures the @ is included
+    @validates('email')
+    def validate_email(self, key, email):
+        assert '@' in email 
+
+        return email
+
+
