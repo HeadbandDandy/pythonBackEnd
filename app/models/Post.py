8756ea1754d6cc587804599f73a4141a510c9f7b
Darryl Johnson <dandycodes@gmail.com>
2022-11-30 13:55:30 -0500

    Post file contains Post import and includes "USER"

--- app/models/Post.py
+++ app/models/Post.py
@@ -10,10 +10,10 @@ from sqlalchemy.orm import relationship
 
 class Post(Base):
     __tablename__ = 'posts'
+    user = relationship('User')
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
     post_url = Column(String(100), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    
