53f9aa074d341ef7b0d2081b62e0dff7021b069f
Darryl Johnson <dandycodes@gmail.com>
2022-11-30 13:55:08 -0500

    Post and seed files updated

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,4 @@
 # below will import User class from User.py file
 
 from .User import User
+from .Post import Post
--- seeds.py
+++ seeds.py
@@ -1,7 +1,7 @@
 # This file uses Base class with engine connection to drop all existing tables
 # Secondly this file will create any file that Base mappped
 
-from app.models import User
+from app.models import User, Post
 from app.db import Session, Base, engine
 
 
@@ -21,6 +21,18 @@ db.add_all([
   User(username='johnnygirl14', email='ihellier3@google.jp', password='password123'),
   User(username='iamsenorfrog4', email='gmidgley4@weather.com', password='password123'),
   User(username='cheeseanddope', email='notvalid@gmail.com', password='cheese12345')
+])
+
+db.commit()
+
+
+# below are inserted Post Seeds
+db.add_all([
+  Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
+  Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
+  Post(title='Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue', post_url='https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx', user_id=2),
+  Post(title='Nunc purus', post_url='http://desdev.cn/enim/blandit/mi.jpg', user_id=3),
+  Post(title='Pellentesque eget nunc', post_url='http://google.ca/nam/nulla/integer.aspx', user_id=4)
 ])
 
 db.commit()
