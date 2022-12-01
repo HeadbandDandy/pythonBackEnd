3206c3324b6230ececd038c2b5bb89f08aa3bd16
Darryl Johnson <dandycodes@gmail.com>
2022-12-01 13:56:12 -0500

    index function updated for page template

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,9 +1,17 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
+
+
+
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+    
     return render_template('homepage.html')
 
 @bp.route('login')
