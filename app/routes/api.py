4810579af946e11f6be5ab19f0689ed61f65d780
Darryl Johnson <dandycodes@gmail.com>
2022-12-08 13:57:40 -0500

    signup route changed needs testing

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 # Below contains APi endpoints
 import email
-from flask import Blueprint, request
+import json
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -9,12 +10,19 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 
 @bp.route('/users', methods=['POST'])
 def signup():
-    data = request.get_json()
-    
-    newUser = User(
-        username = data.username,
-        email = data.email,
-        password = data.password
-    )
+  data = request.get_json()
+  db = get_db()
 
-    return ''
+  # Below creates a new user
+  newUser = User(
+    username = data['username'],
+    email = data['email'],
+    password = data['password']
+  )
+
+  # below saves above data in database
+  db.add(newUser)
+  db.commit()
+
+  return jsonify(id = newUser.id)
+  
