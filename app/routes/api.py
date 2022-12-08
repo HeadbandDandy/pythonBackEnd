dc7b16c7b9e08eb68de829ff693bed016016ea30
Darryl Johnson <dandycodes@gmail.com>
2022-12-08 13:49:40 -0500

    Post route added to receive data

--- app/routes/api.py
+++ app/routes/api.py
@@ -4,3 +4,9 @@ from app.models import User
 from app.db import get_db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
+
+
+@bp.route('/users', methods=['POST'])
+def signup():
+
+    return ''
