d317e69697dd847e87585b9c86041574035a4249
Darryl Johnson <dandycodes@gmail.com>
2022-12-10 16:19:04 -0500

    utils error

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,11 @@
 from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
-
 # below imports the flask package
 from flask import Flask
 
 def create_app(test_config=None):
 
-  app.register_blueprint(api)   
-  app.jinja_env.filters['format_url'] = filters.format_url
-  app.jinja_env.filters['format_date'] = filters.format_date
-  app.jinja_env.filters['format_plural'] = filters.format_plural
       # sets up application configuration
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
@@ -27,6 +22,10 @@ def create_app(test_config=None):
   # below registers the "home, dashboard routes" blueprint 
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
+  app.register_blueprint(api)   
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   init_db(app)
   
