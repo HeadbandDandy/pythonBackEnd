070e203c590e568baca746aaaa5baa5c9af02b45
Darryl Johnson <dandycodes@gmail.com>
2022-12-10 13:02:31 -0500

    route created data not showing in DOM

--- app/routes/api.py
+++ app/routes/api.py
@@ -108,4 +108,29 @@ def upvote():
     return jsonify(message = 'Upvote failed'), 500
 
   return '', 204
+
+# below contains route for users to create a POST
+
+@bp.route('/posts', methods=['POST'])
+def create():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    # create a new post
+    newPost = Post(
+      title = data['title'],
+      post_url = data['post_url'],
+      user_id = session.get('user_id')
+    )
+
+    db.add(newPost)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+
+    db.rollback()
+    return jsonify(message = 'Post failed'), 500
+
+  return jsonify(id = newPost.id)
         
